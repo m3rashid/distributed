3995977dd46b19aff13867bc6bb255eb0f38169f
m3rashid <m3rashid.hussain@gmail.com>
2024-04-01 23:40:09 +0530

    update

--- cmd.sh
+++ cmd.sh
@@ -5,15 +5,7 @@ cmd_line_args=("$@")
 declare -a allowed_cmds=("gen" "clean" "init")
 declare -a folders_to_clean=("node_modules" "build" "dist" ".swc" ".gradle")
 
-for i in "${cmd_line_args[@]}"
-do
-	if [[ ! " ${allowed_cmds[@]} " =~ " ${i} " ]]; then
-		echo "Invalid command: $i"
-		echo "Allowed commands: ${allowed_cmds[@]}"
-		exit 1
-	fi
-done
-
+# function to check if an element is in an array
 function contains() {
 	local n=$#
 	local value=${!n}
@@ -25,6 +17,38 @@ function contains() {
 	return 1
 }
 
+# if no command is passed, error
+if [ $# -eq 0 ]; then
+	echo "No command passed"
+	echo "Allowed commands: ${allowed_cmds[@]}"
+	exit 1
+fi
+
+# if more than one command is passed, error
+if [ $# -gt 1 ]; then
+	echo "Only one command is allowed"
+	echo "Allowed commands: ${allowed_cmds[@]}"
+	exit 1
+fi
+
+# if command is not allowed, error
+cmd_allowed=false
+for i in "${allowed_cmds[@]}"
+do
+	if [ $1 == $i ]; then
+		cmd_allowed=true
+		break
+	fi
+done
+
+
+if ! $cmd_allowed; then
+	echo "Invalid command: $1"
+	echo "Allowed commands: ${allowed_cmds[@]}"
+	exit 1
+fi
+
+# function to clean the project
 function clean() {
 	make remove-gen-proto
 	make remove-gen-types
@@ -35,19 +59,20 @@ function clean() {
 	done
 }
 
+# function to generate the project
 function generate () {
 	make gen-proto
 	make gen-types
 }
 
-if contains "$@" "clean"; then
+if contains $cmd_line_args "clean"; then
 	clean
 fi
 
-if contains "$@" "gen"; then
+if contains $cmd_line_args "gen"; then
 	generate
 fi
 
-if contains "$@ " "init"; then
+if contains $cmd_line_args "init"; then
 	generate
 fi
